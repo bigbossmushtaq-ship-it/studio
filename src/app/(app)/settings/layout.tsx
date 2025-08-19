9bcc17602d9bdf3bef150280b3b9cd17e426121b
Adnan Mushtaq <bigbossmushtaq@gmail.com>
2025-08-19 06:48:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/settings/layout.tsx
+++ src/app/(app)/settings/layout.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 
 import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
 
 interface SettingsLayoutProps {
   children: React.ReactNode
