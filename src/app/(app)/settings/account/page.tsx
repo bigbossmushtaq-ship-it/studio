c923c9775882ee3876dbf9cd9af3a2ec87ab4c39
Adnan Mushtaq <bigbossmushtaq@gmail.com>
2025-08-19 10:20:03 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardTitle>
    
    with

--- src/app/(app)/settings/account/page.tsx
+++ src/app/(app)/settings/account/page.tsx
@@ -45,28 +45,6 @@ export default function AccountSettingsPage() {
   return (
       <div className="space-y-8">
         <Card>
-            <CardHeader>
-                <CardTitle>Profile Picture</CardTitle>
-            </CardHeader>
-            <CardContent className="flex flex-col items-center gap-4">
-                <Avatar className="h-32 w-32">
-                    <AvatarImage src={profilePic} alt="Profile Picture" />
-                    <AvatarFallback>{username.charAt(0)}</AvatarFallback>
-                </Avatar>
-                <input
-                    type="file"
-                    ref={fileInputRef}
-                    onChange={handleProfilePicChange}
-                    className="hidden"
-                    accept="image/*"
-                />
-                <Button variant="outline" onClick={handleUploadClick}>
-                    <Pencil className="mr-2 h-4 w-4" />
-                    Upload new picture
-                </Button>
-            </CardContent>
-        </Card>
-        <Card>
           <CardHeader>
             <CardTitle>Account Details</CardTitle>
           </CardHeader>
