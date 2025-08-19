c42868271e203e7a417eaf899fb94db1947ab301
Adnan Mushtaq <bigbossmushtaq@gmail.com>
2025-08-19 17:35:40 +0000

    <!DOCTYPE html> <html> <head>   <title>Firebase File Upload</title>   <s

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,19 +3,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { uploadBytesResumable } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "projectId": "tuneflow-meg7z",
-  "appId": "1:772294500650:web:c977452b7c0d3ad7c77e53",
-  "storageBucket": "tuneflow-meg7z.appspot.com",
-  "apiKey": "AIzaSyDzosPxByJmbb-7xVzaKEgGOF8OixcGNj0",
-  "authDomain": "tuneflow-meg7z.firebaseapp.com",
-  "messagingSenderId": "772294500650"
+  apiKey: "AIzaSyDzosPxByJmbb-7xVzaKEgGOF8OixcGNj0",
+  authDomain: "tuneflow-meg7z.firebaseapp.com",
+  projectId: "tuneflow-meg7z",
+  storageBucket: "tuneflow-meg7z.appspot.com",
+  messagingSenderId: "772294500650",
+  appId: "1:772294500650:web:c977452b7c0d3ad7c77e53"
 };
 
 // Initialize Firebase
@@ -26,7 +22,3 @@ export const db = getFirestore(app);
 
 // Get a reference to the storage service
 export const storage = getStorage(app);
-
-    
-
-    
